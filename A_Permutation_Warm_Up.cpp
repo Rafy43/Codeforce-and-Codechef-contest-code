814dadab745bb2ede55ec5d8b2660f7b03bad18d
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-05-01 21:31:56 +0600

    Add files via upload

---
+++ A_Permutation_Warm_Up.cpp
@@ -0,0 +1,15 @@
+//01.05.2025
+//rafy
+//permutation
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int f=ceil((n*n)/4)+1;
+        cout<<f<<endl;
+    }
+}
